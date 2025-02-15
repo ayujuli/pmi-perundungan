485482900253e15080a31d550521b717e01183d9
ayujuli <akuntugasayu@gmail.com>
2025-02-15 12:54:42 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5y7qGQQgtWl":
+        Script1();
+        break;
+      case "5c4rPT9qcl5":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+
+}
+
